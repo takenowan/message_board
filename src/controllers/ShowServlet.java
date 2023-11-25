8fa10f5389373e039b243da27fb23482207207e2
hayato <hayato@LAPTOP-GAJB8TQT>
2023-11-26 03:04:49 +0900

    ShowServlet.javaを作成

--- src/controllers/ShowServlet.java
+++ src/controllers/ShowServlet.java
@@ -10,8 +10,7 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
-import com.google.protobuf.Message;
-
+import models.Message;
 import utils.DBUtil;
 
 /**
@@ -19,8 +18,8 @@ import utils.DBUtil;
  */
 @WebServlet("/show")
 public class ShowServlet extends HttpServlet {
-	private static final long serialVersionUID = 1L;
-       
+    private static final long serialVersionUID = 1L;
+
     /**
      * @see HttpServlet#HttpServlet()
      */
@@ -29,9 +28,9 @@ public class ShowServlet extends HttpServlet {
         // TODO Auto-generated constructor stub
     }
 
-	/**
-	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
-	 */
+    /**
+     * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+     */
     protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
         EntityManager em = DBUtil.createEntityManager();
 
